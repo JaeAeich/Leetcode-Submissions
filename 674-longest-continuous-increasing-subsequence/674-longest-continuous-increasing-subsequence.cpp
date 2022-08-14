600b175e0c6a45762869f5a79b42ff3081be68fa
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-14 16:59:46 +0530

    Time: 10 ms (85.71%), Space: 11.1 MB (17.55%) - LeetHub

--- 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.cpp
+++ 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.cpp
@@ -3,11 +3,8 @@ public:
     int findLengthOfLCIS(vector<int>& v) {
         int len=0,ans=0;
         for(int i=0;i<v.size();i++){
-            if(i==0 || v[i-1]<v[i]){
-                len++;
-            }
+            if(i==0 || v[i]>v[i-1]) ans=max(ans,++len);
             else len=1;
-            ans=max(ans,len);
         }
         return ans;
     }
