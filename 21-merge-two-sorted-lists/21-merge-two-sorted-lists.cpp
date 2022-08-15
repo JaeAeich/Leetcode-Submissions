cfb94c46a68f0de5757533ec6cc964384474c494
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-15 18:37:03 +0530

    Time: 17 ms (26.10%), Space: 15 MB (7.87%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -25,50 +25,122 @@ public:
     //     }
     // }
     
-    //iterative solution
+//     //iterative solution
+//     ListNode* mergeTwoLists(ListNode* l1,ListNode* l2){
+//         ListNode *ans,*c,*n;
+//         if(!l1){
+//             return l2;
+//         }
+        
+//         if(!l2){
+//             return l1;
+//         }
+//         if(!l1 && !l2){
+//             return NULL;
+//         }
+        
+//         if(l1->val<l2->val){
+//             ans=l1;
+//             c=l1;
+//             n=l2;
+//         }
+//         else {
+//             ans=l2;
+//             c=l2;
+//             n=l1;
+//         }
+//         while(c && n){
+//             ListNode* temp=c->next;
+            
+//             if(!c->next){
+//                 c->next=n;
+//                 break;
+//             }
+            
+//             if(c->next->val<n->val){
+//                 c=c->next;
+//             }
+//             else{
+//                 c->next=n;
+//                 n=temp;
+//                 c=c->next;
+//             }
+//         }
+//         return ans;
+//     }
+    //creating a new list
+    void pushtail(ListNode* &tail,int data){
+        ListNode* temp=new ListNode(data);
+        if(tail){
+            tail->next=temp;
+        }
+        tail=temp;
+    }
+    
     ListNode* mergeTwoLists(ListNode* l1,ListNode* l2){
-        ListNode *ans,*c,*n;
-        if(!l1){
+        if(l1==NULL){
             return l2;
         }
-        
-        if(!l2){
+        if(l2==NULL){
             return l1;
         }
         if(!l1 && !l2){
             return NULL;
         }
-        
+        ListNode* ans;
         if(l1->val<l2->val){
-            ans=l1;
-            c=l1;
-            n=l2;
+            ans=new ListNode(l1->val);
+            l1=l1->next;
         }
-        else {
-            ans=l2;
-            c=l2;
-            n=l1;
+        else{
+            ans=new ListNode(l2->val);
+            l2=l2->next;
         }
-        while(c && n){
-            ListNode* temp=c->next;
-            
-            if(!c->next){
-                c->next=n;
+        ListNode* tail=ans;
+        while(l1 || l2){
+            if(!l1){
+                tail->next=l2;
+                break;
+            }
+            if(!l2){
+                tail->next=l1;
                 break;
             }
             
-            if(c->next->val<n->val){
-                c=c->next;
+            if(l1->val>l2->val){
+                pushtail(tail,l2->val);
+                l2=l2->next;
             }
             else{
-                c->next=n;
-                n=temp;
-                c=c->next;
+                pushtail(tail,l1->val);
+                l1=l1->next;
             }
         }
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
