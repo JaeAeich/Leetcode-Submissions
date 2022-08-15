422140c0337517bf74be5ab2cb16b1968c0f19ce
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-15 18:56:57 +0530

    Time: 7 ms (37.01%), Space: 6.7 MB (26.43%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,27 +1,55 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
+    // //Brute force
+    // char findTheDifference(string s, string t) {
+    //     map<char,int> m;
+    //     for(int i=0;s[i]!='\0';i++){
+    //         if(m.find(s[i])==m.end()){
+    //             m[s[i]]=1;
+    //         }
+    //         else m[s[i]]++;
+    //     }
+    //     char ret;
+    //     for(int i=0;t[i]!='\0';i++){
+    //         if(m.find(t[i])==m.end()){
+    //             ret =t[i];
+    //             break;
+    //         }
+    //         else {
+    //             if(m[t[i]]) m[t[i]]--;
+    //             else {
+    //                 ret=t[i];
+    //                 break;
+    //             }
+    //         }
+    //     }
+    //     return ret ;
+    // }
+    
+    //Xor method
     char findTheDifference(string s, string t) {
-        map<char,int> m;
+        sort(begin(s),end(s));
+        sort(begin(t),end(t));
+        char ret=t.back();
         for(int i=0;s[i]!='\0';i++){
-            if(m.find(s[i])==m.end()){
-                m[s[i]]=1;
-            }
-            else m[s[i]]++;
-        }
-        char ret;
-        for(int i=0;t[i]!='\0';i++){
-            if(m.find(t[i])==m.end()){
+            int a=(int)s[i];
+            int b=(int)t[i];
+            if(a^b){
                 ret =t[i];
                 break;
             }
-            else {
-                if(m[t[i]]) m[t[i]]--;
-                else {
-                    ret=t[i];
-                    break;
-                }
-            }
         }
-        return ret ;
+        return ret;
     }
 };
+
+
+
+
+
+
+
+
+
+
