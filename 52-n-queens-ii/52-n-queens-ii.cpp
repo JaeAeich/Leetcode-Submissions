8d8bd54e473e8a266a5083b87268991488130453
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-16 20:11:31 +0530

    Time: 11 ms (44.21%), Space: 6.4 MB (42.06%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    set<vector<string>> finans={};
+    
+    bool isSafe(vector<string> &v,int &n, int row, int col){
+        if(row<0 || col<0 || row>=n || col>=n) return false;
+        for(int i=0;i<row;i++){
+            if(v[i][col] =='Q') return false;
+        }
+        for(int i=row-1,j=col-1;i>=0 && j>=0;--i,--j){
+            if(v[i][j]=='Q') return false;
+        }
+        for(int i=row-1,j=col+1;i>=0 && j<n;--i,++j){
+            if(v[i][j]=='Q') return false;
+        }
+        return true;
+    }
+    
+    void helper(vector<string> &v, int &n, int i, int j){
+        if(i>=n){
+            finans.insert(v);
+            return;
+        }
+        if(j>=n) return;
+        
+        if(isSafe(v,n,i,j)){
+            v[i][j]='Q';
+            helper(v,n,i+1,0);
+            v[i][j]='.';
+            helper(v,n,i,j+1);
+        }
+        else{
+            helper(v,n,i,j+1);
+        }
+    }
+    
+    void solveNQueens(int n) {
+        vector<string> board;
+        for(int i=0;i<n;i++){
+            string temp;
+            for(int j=0;j<n;j++){
+                temp+='.';
+            }
+            board.push_back(temp);
+        }
+        helper(board,n,0,0);
+    }
+    int totalNQueens(int n) {
+        solveNQueens(n);
+        return finans.size();
+    }
+};
