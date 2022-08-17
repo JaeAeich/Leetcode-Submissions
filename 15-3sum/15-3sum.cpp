dd9c6f84431f1c7b07538ffd0c075eb274bcbed7
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-17 15:17:37 +0530

    Time: 2444 ms (5.00%), Space: 183.9 MB (6.01%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& v) {
-        set<vector<int>> s;
-        vector<vector<int>> ans;
+        set<vector<int>> s={};
+        vector<vector<int>> ans={};
         sort(v.begin(),v.end());
         for(int i=0;i<v.size()-2;i++){
-            // cout<<"iteration: "<<i+1<<endl;
+            if(v[i]>0) break;
+            if(v[i]==v[i+1] && i<v.size()-2 && v[i]>0) i++;
             for(int j=i+1,k=v.size()-1;j<k;){
                 int sum=v[i]+v[j]+v[k];
-                // cout<<v[i]<<" "<<v[j]<<" "<<v[k]<<endl;
                 if(sum==0){
                     s.insert({v[i],v[j],v[k]});
                     ++j;
@@ -22,6 +22,7 @@ public:
         for(vector<int> vec: s){
             ans.push_back(vec);
         }
+        
         return ans;
     }
 };
