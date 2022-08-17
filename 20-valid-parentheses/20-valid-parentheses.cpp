59d76621a3c899bf7568ddac8d6db27606b3acd4
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-17 19:27:02 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.24%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,11 +2,6 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        unordered_map<char,char> m;
-        // map closing backets
-        m['{']='}';
-        m['(']=')';
-        m['[']=']';
         //push brackets in stack if its opening bracket.
         for(int i=0;s[i]!='\0';i++){
             if(s[i]=='(' || s[i]=='{' || s[i]=='['){
@@ -14,14 +9,14 @@ public:
             }
             else{
                 // if its not opening and top is its counter part then pop from stack. if stack is empty you cant push closing bracket so return false.
+                char top=st.top();
                 if(st.empty()) return false;
-                if(m[st.top()]==s[i]){
+                if(top=='(' && s[i]==')' || top=='{' && s[i]=='}'||top=='[' && s[i]==']'){
                     st.pop();
                 }
                 else return false;
             }
         }
-        if(st.empty()) return true;
-        return false;
+        return st.empty();
     }
 };
