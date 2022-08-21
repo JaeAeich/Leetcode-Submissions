d094ea7067d8c954ff361300547d7bbf1b7709c3
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-21 22:28:55 +0530

    Time: 35 ms (66.01%), Space: 24.1 MB (63.84%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -3,10 +3,13 @@ public:
     vector<int> productExceptSelf(vector<int>& v) {
         long long int p=1,c=0;
         for(int i=0;i<v.size();i++){
+            // taking product of all nums but zero.
             if(v[i]) p*=v[i];
+            //counting number of zeroes.
             else c++;
         }
         if(c<2){
+            // If on ezero is present then except that zero all ele will be zero.
             if(c==1){
                 for(int i=0;i<v.size();i++){
                     if(!v[i]){
@@ -14,15 +17,15 @@ public:
                     }
                     else v[i]=0;
                 }
-                return v;
             }
+            // if number of zeroes is 1 then each ele=total product/ele.
             if(c==0){
                 for(int i=0;i<v.size();i++){
                     v[i]=p/v[i];
                 }
-                return v;
             }
         }
+        // if there are more than 2 zeroes, all ele will become zeroes.
         else{
             for(int i=0;i<v.size();i++){
                 v[i]=0;
