a274fa5ef2c617076abb0f8a9f559f1f18426844
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-23 22:42:29 +0530

    Time: 258 ms (18.04%), Space: 93.4 MB (52.40%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,22 +1,61 @@
 class Solution {
 public:
-    //Time complexity :- O(N)
-    //Space complexity :-O(N)
-    int maxProfit(vector<int>& v) {
-        int n=v.size(),max_yet=v[n-1];
-        vector<int> max_ele(n);
-        //Storing maximum ele after ith index of array.
-        for(int i=n-1;i>=0;i--){
-            if(v[i]>=max_yet){
-                max_yet=v[i];
+    // //Time complexity :- O(N)
+    // //Space complexity :-O(N)
+    // int maxProfit(vector<int>& v) {
+    //     int n=v.size(),max_yet=v[n-1];
+    //     vector<int> max_ele(n);
+    //     //Storing maximum ele after ith index of array.
+    //     for(int i=n-1;i>=0;i--){
+    //         if(v[i]>=max_yet){
+    //             max_yet=v[i];
+    //         }
+    //         max_ele[i]=max_yet;
+    //     }
+    //     int ans=INT_MIN;
+    //     //checking for all index, max profit.
+    //     for(int i=0;i<n;i++){
+    //         ans=max(max_ele[i]-v[i],ans);
+    //     }
+    //     return ans;
+    // }
+    
+    // Time complexity:- O(N), Space complexity:- O(1)
+    int maxProfit(vector<int>& v){
+        int least_till_now=v[0],max_prof=INT_MIN;
+        for(int i=0;i<v.size();i++){
+            if(v[i]<least_till_now){
+                least_till_now=v[i];
             }
-            max_ele[i]=max_yet;
+            
+            int prof_today=v[i]-least_till_now;
+            
+            if(max_prof<prof_today){
+                max_prof=prof_today;
+            }
         }
-        int ans=INT_MIN;
-        //checking for all index, max profit.
-        for(int i=0;i<n;i++){
-            ans=max(max_ele[i]-v[i],ans);
-        }
-        return ans;
+        return max_prof;
     }
+    
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
