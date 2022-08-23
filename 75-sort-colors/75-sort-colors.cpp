a3c4169ca7bcc4cc027204d9ce11c019d6d33e48
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-23 17:52:30 +0530

    Time: 3 ms (64.11%), Space: 8.3 MB (23.98%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
+    // Selection sort O(N^2).
     void sortColors(vector<int>& v) {
         int n=v.size();
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
+                //swap the greater element with the smallest one.
                 if(v[j]<v[i]){
-                    swap(v[j],v[i]);
+                    v[i]^=v[j];
+                    v[j]^=v[i];
+                    v[i]^=v[j];
                 }
             }
         }
