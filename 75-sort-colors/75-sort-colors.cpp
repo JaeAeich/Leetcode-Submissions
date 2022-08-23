2a046fc6de16a313b359624def24d4bab6c963f1
JaeHabib <100477031+JaeHabib@users.noreply.github.com>
2022-08-23 18:58:54 +0530

    Time: 3 ms (64.11%), Space: 8.3 MB (70.57%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,14 +1,32 @@
 class Solution {
 public:
-    // Selection sort O(N^2).
+    // // Selection sort O(N^2).
+    // void sortColors(vector<int>& v) {
+    //     int n=v.size();
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i;j<n;j++){
+    //             //swap the greater element with the smallest one.
+    //             if(v[j]<v[i]){
+    //                 swap(v[i],v[j]);
+    //             }
+    //         }
+    //     }
+    // }
+    
+    // Count Sort O(N)
     void sortColors(vector<int>& v) {
-        int n=v.size();
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                //swap the greater element with the smallest one.
-                if(v[j]<v[i]){
-                    swap(v[i],v[j]);
-                }
+        vector<int> count={0,0,0};
+        // count the frequecy of each distinc element.
+        for(int i=0;i<v.size();i++){
+            count[v[i]]++;
+        }
+        int j=0;
+        // Till freq of ith element becomes zero push that into the vector.
+        for(int i=0;i<3;i++){
+            while(count[i]){
+                v[j]=i;
+                j++;
+                count[i]--;
             }
         }
     }
