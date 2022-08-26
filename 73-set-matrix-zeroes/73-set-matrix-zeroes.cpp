629390f9bd9a0bfcc3814ed1c665e83a1430dae2
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-08-26 21:57:20 +0530

    Time: 20 ms (67.67%), Space: 13.3 MB (53.23%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,24 +1,106 @@
 class Solution {
 public:
+    // //Time complexity:-O(mn), Space complexity :- O(m+n).
+    // void setZeroes(vector<vector<int>>& v) {
+    //     vector<pair<int,int>> idx;
+    //     int n=v.size(),m=v[0].size();
+    //     //storing all the index that has zero.
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+    //             if(v[i][j]==0){
+    //                 idx.push_back({i,j});
+    //             }
+    //         }
+    //     }
+    //     //making ele of rows and cols zero.
+    //     for(auto p:idx){
+    //         for(int  i=0;i<m;i++){
+    //             v[p.first][i]=0;
+    //         }
+    //         for(int i=0;i<n;i++){
+    //             v[i][p.second]=0;
+    //         }
+    //     }
+    // }
+    
+    //Time complexity:- O(mn) , Space complexity:- O(1).
     void setZeroes(vector<vector<int>>& v) {
-        vector<pair<int,int>> idx;
+        bool col=false,row=false;
         int n=v.size(),m=v[0].size();
-        //storing all the index that has zero.
+        for(int i=0;i<m;i++){
+            if(v[0][i]==0){
+                row=true;
+                break;
+            }
+        }
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+            if(v[i][0]==0){
+                col=true;
+                break;
+            }
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(v[i][j]==0){
-                    idx.push_back({i,j});
+                    v[i][0]=v[0][j]=0;
                 }
             }
         }
-        //making ele of rows and cols zero.
-        for(auto p:idx){
-            for(int  i=0;i<m;i++){
-                v[p.first][i]=0;
+        for(int j=1;j<m;j++){
+            if(v[0][j]==0){
+                for(int k=1;k<n;k++){
+                    v[k][j]=0;
+                }
             }
+        }
+        for(int i=1;i<n;i++){
+            if(v[i][0]==0){
+                for(int k=1;k<m;k++){
+                    v[i][k]=0;
+                }
+            }
+        }
+        if(col){
             for(int i=0;i<n;i++){
-                v[i][p.second]=0;
+                v[i][0]=0;
+            }
+        }
+        if(row){
+            for(int i=0;i<m;i++){
+                v[0][i]=0;
             }
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
