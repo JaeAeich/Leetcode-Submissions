f203b029b15caee52f76b69b2a8ccb3ee7c3af86
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-08-29 14:22:45 +0530

    Time: 28 ms (64.70%), Space: 18 MB (20.34%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,17 +2,28 @@ class Solution {
 public:
     const int m=1e4+5;
     
+    // //using hashing
+    // int missingNumber(vector<int>& nums) {
+    //     vector<int> v(m);
+    //     int n=nums.size();
+    //     for(int val:nums){
+    //         v[val]=1;
+    //     }
+    //     for(int i=0;i<n+1;i++){
+    //         if(v[i]==0){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
+    
+    //using xor property
     int missingNumber(vector<int>& nums) {
-        vector<int> v(m);
-        int n=nums.size();
-        for(int val:nums){
-            v[val]=1;
+        int ans=0,i=0;
+        for(;i<nums.size();i++){
+            ans^=nums[i]^i;
         }
-        for(int i=0;i<n+1;i++){
-            if(v[i]==0){
-                return i;
-            }
-        }
-        return -1;
+        ans^=i;
+        return ans;
     }
 };
