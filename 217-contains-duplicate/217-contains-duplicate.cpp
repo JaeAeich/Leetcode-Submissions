e5a72c91e65bc9ad2a52580b992b913466fc6b7d
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-04 22:48:03 +0530

    Time: 126 ms (70.81%), Space: 52.2 MB (35.12%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,9 +1,22 @@
 class Solution {
 public:
+    // //sort
+    // bool containsDuplicate(vector<int>& v) {
+    //     sort(v.begin(),v.end());
+    //     for(int i=0;i<v.size()-1;i++){
+    //         if(v[i]==v[i+1]) return true;
+    //     }
+    //     return false;
+    // }
+    
+    //hash
     bool containsDuplicate(vector<int>& v) {
-        sort(v.begin(),v.end());
-        for(int i=0;i<v.size()-1;i++){
-            if(v[i]==v[i+1]) return true;
+        set<int> s;
+        for(int val:v){
+            if(s.find(val)!=s.end()){
+                return true;
+            }
+            s.insert(val);
         }
         return false;
     }
