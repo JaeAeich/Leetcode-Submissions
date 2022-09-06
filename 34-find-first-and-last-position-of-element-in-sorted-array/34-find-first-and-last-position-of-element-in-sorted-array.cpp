6a1e3ca246868b74d34be40a407634fb0d6bb92b
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-06 16:41:47 +0530

    Time: 15 ms (43.56%), Space: 13.7 MB (63.79%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
-    //Using Stl funct.
-    vector<int> searchRange(vector<int>& v, int t) {
-        //Finding the first occurance of t.
-        auto it=lower_bound(begin(v),end(v),t);
-        int n=v.size();
-        //If t isn't present in v , return {-1,-1}.
-        if(it==v.end() || *it!=t){
-            return {-1,-1};
+    //In binary func true is all the nums greater than or equal to t.
+    //Finding the first t.
+    int lb_firstF(vector<int> &v, int t){
+        int lo=0,hi=v.size()-1;
+        int ans=-1;
+        while(hi>=lo){
+            int mid=lo+(hi-lo)/2;
+            
+            if(v[mid]>=t) ans=mid,hi=mid-1;
+            else lo=mid+1;
         }
+        return ans;
+    }
+    //In binary func true is all the nums less than or equal to t.
+    //Finding the last t.
+    int lb_lastT(vector<int> &v, int t){
+        int lo=0,hi=v.size()-1;
+        int ans=-1;
+        while(hi>=lo){
+            int mid=lo+(hi-lo)/2;
+            
+            if(v[mid]<=t) ans=mid,lo=mid+1;
+            else hi=mid-1;
+        }
+        return ans;
+    }
+    //Binary search
+    vector<int> searchRange(vector<int>& v, int t) {
+        int idx1=lb_firstF(v,t);
+        int idx2=lb_lastT(v,t);
         vector<int> ans;
-        int idx=it-begin(v);
-        ans.push_back(idx);
-        //Finding the last occurance of t.
-        int idx_last=upper_bound(v.begin(),v.end(),t)-begin(v);
-        ans.push_back(--idx_last);
+        
+        if(idx1<v.size() && v[idx1]==t) ans.push_back(idx1);
+        else ans.push_back(-1);
+        if(idx2<v.size() && v[idx2]==t ) ans.push_back(idx2);
+        else ans.push_back(-1);
         return ans;
     }
 };
