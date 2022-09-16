25cfec60189298f91b2c0c5a0ccd72c9a2b9b31d
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-16 16:26:50 +0530

    Time: 179 ms (5.12%), Space: 40.7 MB (5.87%) - LeetHub

---
+++ 89-gray-code/89-gray-code.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    //Genrate gray code using its property.
+    vector<string> genGray(int n){
+        if(n==1) return {"0","1"};
+        
+        vector<string> rev=genGray(n-1);
+        vector<string> str=rev;
+        reverse(begin(rev),end(rev));
+        
+        for(int i=0;i<str.size();i++){
+            str[i]="0"+str[i];
+        }
+        for(int i=0;i<rev.size();i++){
+            rev[i]="1"+rev[i];
+        }
+        str.insert(str.end(),rev.begin(),rev.end());
+        return str;
+    }
+    //Convert gray code string into a number.
+    int makeNum(string s){
+        int ans=0;
+        int mul=1;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s[i]=='1') ans+=mul*1;
+            mul*=2;
+        }
+        return ans;
+    }
+    //Driver code
+    vector<int> grayCode(int n) {
+        vector<int> ans={};
+        vector<string> v=genGray(n);
+        for(string s:v){
+            ans.push_back(makeNum(s));
+        }
+        return ans;
+    }
+};
