b6c33f47cf3c28d19a6072933f03c430b013c4c3
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-17 20:03:39 +0530

    Time: 3 ms (80.67%), Space: 7.6 MB (70.25%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& v) {
-        int aStone=0,bStone=0;
-        int n=v.size();
-        int c=0;
-        for(int i=0,j=n-1;i<=j;){
-            int temp=0;
-            if(v[i]>v[j]){
-                temp=v[i++];
-            } 
-            else temp=v[j--];
-            if(c&1) bStone+=temp;
-            else aStone+=temp;
-        }
-        return aStone>bStone;
+    // //brute force
+    // bool stoneGame(vector<int>& v) {
+    //     int aStone=0,bStone=0;
+    //     int n=v.size();
+    //     int c=0;
+    //     for(int i=0,j=n-1;i<=j;){
+    //         int temp=0;
+    //         if(v[i]>v[j]){
+    //             temp=v[i++];
+    //         } 
+    //         else temp=v[j--];
+    //         if(c&1) bStone+=temp;
+    //         else aStone+=temp;
+    //     }
+    //     return aStone>bStone;
+    // }
+    
+    //Optimal
+    //Note: Alice can always pisk the largest pile, since she is stating first, she will always win.
+    bool stoneGame(vector<int>& v){
+        return true;
     }
 };
