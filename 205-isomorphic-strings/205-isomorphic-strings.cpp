d1780fbcf8ef391f1184df1bc3ef583c1429ed77
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-18 18:36:16 +0530

    Time: 3 ms (97.61%), Space: 7 MB (74.18%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        map<char,char> m;
-        //map all the char to char of other string.
-        if(s.size()!=t.size()) return false;
+        char m1[256]={0},m2[256]={0};
         for(int i=0;i<s.size();i++){
-            if(m.find(s[i])==m.end()){
-                m[s[i]]=t[i];
-            }
-            //If one char get mapped to 2 chars return false.
-            if(m[s[i]]!=t[i]) return false;
-        }
-        map<char,int> mp;
-        //checking if the chars of t are also having one to one mapping.
-        for(auto it=m.begin();it!=m.end();it++){
-            mp[it->second]++;
-            if(mp[it->second]>1) return false; 
+            if(m1[s[i]]!=m2[t[i]]) return false;
+            m1[s[i]]=i+1;
+            m2[t[i]]=i+1;
         }
         return true;
     }
