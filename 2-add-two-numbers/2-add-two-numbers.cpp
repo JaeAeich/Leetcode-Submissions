e4de8511466928e9f27f43d825a72cc26d1fed5d
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-21 10:48:40 +0530

    Time: 57 ms (55.03%), Space: 71.6 MB (12.44%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -11,18 +11,22 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int c=0;//-->carry 
-        ListNode *prevHead=new ListNode(0);
-        ListNode *tail=prevHead;
-        
-        while(c || l1 || l2){
-            c+=(l1?l1->val:0)+(l2?l2->val:0);
-            tail->next=new ListNode(c%10);//-->since total sum mod 10 is val we need to store.
-            tail=tail->next;
-            c/=10; // --> carry in decimal sys is numbers right shift 
+        ListNode* ans=new ListNode(0),*dummy=ans;
+        int c=0; // -->carry from the sum.
+        while(l1 || l2){
+            int sum=(l1?l1->val:0)+(l2?l2->val:0)+c;
+            if(sum>9){
+                c=1;
+                sum=sum-10;
+            }
+            else c=0;
+            ListNode* temp=new ListNode(sum);
+            dummy->next=temp;
+            dummy=temp;
             if(l1) l1=l1->next;
             if(l2) l2=l2->next;
         }
-        return prevHead->next;
+        if(c) dummy->next=new ListNode(c);
+        return ans->next;
     }
 };
