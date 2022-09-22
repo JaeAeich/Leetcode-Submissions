b9b4c89e2703b79dea15c70d7be327569369ca16
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-22 22:18:54 +0530

    Time: 15 ms (18.39%), Space: 16.7 MB (19.07%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -5,18 +5,18 @@ public:
     void helper(vector<int> &v,int i=0,vector<int> ans={}){
         if(i==v.size()){
             sort(begin(ans),end(ans));
-            if(find(begin(finans),end(finans),ans)==finans.end()){
-                finans.push_back(ans);
-            }
+            finans.push_back(ans);
             return;
         }
         ans.push_back(v[i]);
         helper(v,i+1,ans);
+        while(i+1<v.size() && v[i]==v[i+1]) i+=1;
         ans.pop_back();
         helper(v,i+1,ans);
     }
     
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         helper(nums);
         return finans;
     }
