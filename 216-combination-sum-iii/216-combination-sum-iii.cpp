034fd180d3497cf7f01a5d5be7398362c785b939
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-24 13:32:48 +0530

    Time: 4 ms (39.08%), Space: 6.9 MB (21.29%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,28 +1,72 @@
 class Solution {
 public:
+//     //attempt 1
     vector<vector<int>> finans;
     
-    void helper(int k, int n,vector<int> vis,int i=1,int sum=0,vector<int> ans={}){
-        if(sum>k || ans.size()>n) return ;
-        if(sum==k && ans.size()==n){
-            finans.push_back(ans);
-            return;
-        }
-        if(i>=vis.size()) return;
+//     void helper(int k, int n,vector<int> vis,int i=1,int sum=0,vector<int> ans={}){
+//         if(sum>k || ans.size()>n) return ;
+//         if(sum==k && ans.size()==n){
+//             finans.push_back(ans);
+//             return;
+//         }
+//         if(i>=vis.size()) return;
         
-        if(vis[i]){
-            vis[i]=0;
+//         if(vis[i]){
+//             vis[i]=0;
+//             ans.push_back(i);
+//             helper(k,n,vis,i+1,sum+i,ans);
+//             vis[i]=1;
+//             ans.pop_back();
+//         }
+        
+//         helper(k,n,vis,i+1,sum,ans);
+//     }
+//     vector<vector<int>> combinationSum3(int k, int n) {
+//         vector<int> vis(10,1);
+//         helper(n,k,vis);
+//         return finans;
+//     }
+    
+    void dfs(int k, int n,int val=1,int sum=0,vector<int> ans={}){
+        if(ans.size()>k || sum>n)return ;
+        
+        if(sum==n && ans.size()==k){
+            finans.push_back(ans);
+        }
+        
+        for(int i=val;i<10;++i){
             ans.push_back(i);
-            helper(k,n,vis,i+1,sum+i,ans);
-            vis[i]=1;
+            dfs(k,n,i+1,sum+i,ans);
             ans.pop_back();
         }
-        
-        helper(k,n,vis,i+1,sum,ans);
     }
+    
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> vis(10,1);
-        helper(n,k,vis);
+        dfs(k,n);
         return finans;
     }
+    
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
