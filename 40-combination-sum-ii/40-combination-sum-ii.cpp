4fa40721c9c10de926e0a3e3fa74f913845944ec
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-24 13:44:42 +0530

    Time: 78 ms (8.67%), Space: 16.5 MB (11.45%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> finans;
+    
+    void helper(vector<int> &v, int t, int idx=0,vector<int> ans={}){
+        if(t<0 || idx>v.size()) return;
+        if(t==0){
+            finans.push_back(ans);
+            return ;
+        }
+        
+        for(int i=idx;i<v.size();++i){
+            ans.push_back(v[i]);
+            helper(v,t-v[i],i+1,ans);
+            while(i+1<v.size() && v[i]==v[i+1]) i++;
+            ans.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& v, int t) {
+        sort(begin(v),end(v));
+        helper(v,t);
+        return finans;
+    }
+};
