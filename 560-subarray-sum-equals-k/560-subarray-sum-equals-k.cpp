4134caf582eb25274e8349a125ccffaf477ad5d1
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-09-27 11:09:44 +0530

    Time: 124 ms (74.26%), Space: 43.9 MB (29.91%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,22 +1,50 @@
 class Solution {
 public:
     int subarraySum(vector<int>& v, int k) {
+        // int n=v.size();
+        // int c=0;
+        // int sum=0;
+        // //Hash map that will store frequency of all the prefix sum.
+        // map<int,int> m;
+        // //Pushing 0 becuase subarray can start from index .
+        // m[0]=1;
+        // for(int i=0;i<n;i++){
+        //     sum+=v[i];
+        //     //If we have sum-k in Hashmap, then our total count will be incremented by that frequency. 
+        //     if(m.find(sum-k)!=m.end()){
+        //         c+=m[sum-k];
+        //     }
+        //     //Else push the prefix sum into the Hashmap or increase its freq if already present.
+        //     m[sum]++;
+        // }
+        // return c;
+        
         int n=v.size();
-        int c=0;
-        int sum=0;
-        //Hash map that will store frequency of all the prefix sum.
         map<int,int> m;
-        //Pushing 0 becuase subarray can start from index .
         m[0]=1;
+        int sum=0,c=0;
         for(int i=0;i<n;i++){
             sum+=v[i];
-            //If we have sum-k in Hashmap, then our total count will be incremented by that frequency. 
             if(m.find(sum-k)!=m.end()){
                 c+=m[sum-k];
             }
-            //Else push the prefix sum into the Hashmap or increase its freq if already present.
             m[sum]++;
         }
         return c;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
