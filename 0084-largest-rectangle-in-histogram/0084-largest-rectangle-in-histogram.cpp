9c0edd61496451eed3086e509793bf410bb58854
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-10-31 13:11:43 +0530

    Time: 390 ms (51.38%), Space: 81.5 MB (39.92%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -15,28 +15,42 @@ public:
     //     }
     //     return ans;
     // }    
-    int largestRectangleArea(vector<int>& v) {
-        int n=v.size();
+    int largestRectangleArea(vector<int> &v)
+    {
+        int n = v.size();
         stack<int> s;
-        vector<int> nle(n,-1);
-        vector<int> ple(n,-1);
-        for(int i=0;i<v.size();++i){
-            while(!s.empty() && v[s.top()]>=v[i]){
-                nle[s.top()]=i;
+        vector<int> nle(n, -1);
+        vector<int> ple(n, -1);
+        // looping ele to generate ple and nle
+        for (int i = 0; i < v.size(); ++i)
+        {
+            while (!s.empty() && v[s.top()] >= v[i])
+            {
+                nle[s.top()] = i;
                 s.pop();
             }
-            if(!s.empty()) ple[i]=s.top();
+            if (!s.empty())
+                ple[i] = s.top();
             s.push(i);
         }
-        int ans=INT_MIN;
-        for(int i=0;i<v.size();i++){
-            int n=nle[i];
-            int p=ple[i];
-            int f=n-p-1;
-            if(p==-1 && n!=-1) f=n;
-            else if(p!=-1 && n==-1) f=v.size()-p-1;
-            else if(n==-1 && p==-1) f=v.size();
-            ans=max(ans,(v[i]*f));
+        // initiating ans variable to min to find max of all the area.
+        int ans = INT_MIN;
+        for (int i = 0; i < v.size(); i++)
+        {
+            int n = nle[i];    //-->next idx of lesser ele than ith ele of vector.
+            int p = ple[i];    //-->prev idx of lesser ele that ith ele of vector.
+            int f = n - p - 1; //-->if the ple and nle exist the width will be n-p-1.
+
+            // likewise diff senario depending on existance on ple and nle..
+            if (p == -1 && n != -1)
+                f = n;
+            else if (p != -1 && n == -1)
+                f = v.size() - p - 1;
+            else if (n == -1 && p == -1)
+                f = v.size();
+
+            // finding the max of the areas calculated.
+            ans = max(ans, (v[i] * f));
         }
         return ans;
     }
