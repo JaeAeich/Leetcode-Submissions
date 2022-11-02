24c199d9f93e7eae5f847f23f9fb6327b19d5648
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-11-02 15:22:45 +0530

    Time: 107 ms (6.57%), Space: 15.3 MB (9.71%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& v) {
+        // making a sorted vector to compare the unsorte vec to.
         vector<int> s;
         s=v;
         sort(begin(s),end(s));
         int c=0;
-        
+        //frequency hash map for sorted and unsorted vec.
         unordered_map<int,int> f;
         unordered_map<int,int> fs;
         
         for(int i=0;i<v.size();i++){
+            //keeping tab of freq of ele of sorted and unsorted vec.
             f[v[i]]++;
             fs[s[i]]++;
+            //if at any point the freq becomes equal , that means it containts a chunk.
             if(f==fs) {
                 c++;
-                f={};
-                fs={};
             }
         }
         
