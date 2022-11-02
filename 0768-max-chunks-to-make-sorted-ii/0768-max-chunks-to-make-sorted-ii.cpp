becae2d68f32159547f58479f7c5e3f805448b95
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-11-02 15:31:02 +0530

    Time: 15 ms (77.84%), Space: 12.4 MB (56.08%) - LeetHub

--- 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
+++ 0768-max-chunks-to-make-sorted-ii/0768-max-chunks-to-make-sorted-ii.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
+//     //using freq array
+//     int maxChunksToSorted(vector<int>& v) {
+//         // making a sorted vector to compare the unsorte vec to.
+//         vector<int> s;
+//         s=v;
+//         sort(begin(s),end(s));
+//         int c=0;
+//         //frequency hash map for sorted and unsorted vec.
+//         unordered_map<int,int> f;
+//         unordered_map<int,int> fs;
+        
+//         for(int i=0;i<v.size();i++){
+//             //keeping tab of freq of ele of sorted and unsorted vec.
+//             f[v[i]]++;
+//             fs[s[i]]++;
+//             //if at any point the freq becomes equal , that means it containts a chunk.
+//             if(f==fs) {
+//                 c++;
+//             }
+//         }
+        
+//         return c;
+//     }
     int maxChunksToSorted(vector<int>& v) {
-        // making a sorted vector to compare the unsorte vec to.
-        vector<int> s;
-        s=v;
+        vector<int> s=v;
         sort(begin(s),end(s));
         int c=0;
-        //frequency hash map for sorted and unsorted vec.
-        unordered_map<int,int> f;
-        unordered_map<int,int> fs;
-        
+        long long int sum_v=0,sum_s=0;
         for(int i=0;i<v.size();i++){
-            //keeping tab of freq of ele of sorted and unsorted vec.
-            f[v[i]]++;
-            fs[s[i]]++;
-            //if at any point the freq becomes equal , that means it containts a chunk.
-            if(f==fs) {
+            sum_v+=v[i];
+            sum_s+=s[i];
+            if(sum_s==sum_v){
                 c++;
+                sum_s=0;
+                sum_v=0;
             }
         }
-        
         return c;
     }
+    
 };
