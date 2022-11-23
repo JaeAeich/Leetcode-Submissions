52ae165235674c5e65f120c4aa80bd3baba67064
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-11-23 21:41:05 +0530

    Time: 159 ms (51.91%), Space: 49.7 MB (21.50%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -2,8 +2,19 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-// using inbuilt function
+// // using inbuilt function
+// var reverseString = function(s) {
+//     s.reverse();
+//     return s;
+// };
+
 var reverseString = function(s) {
-    s.reverse();
+    let i=0,j=s.length-1;
+    
+    while(i<j){
+        [s[i],s[j]]=[s[j],s[i]];
+        i++;
+        j--;
+    }
     return s;
-};
+}
