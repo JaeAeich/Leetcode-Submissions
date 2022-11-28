0c4cf2c66472f028bb1e8b21b5b2157fdfe0f86b
JaeAeich <100477031+JaeAeich@users.noreply.github.com>
2022-11-28 13:55:04 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (72.36%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,32 +1,53 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& v) {
-        int t=0,b=v.size()-1,l=0,r=v[0].size()-1;
+    vector<int> spiralOrder(vector<vector<int>> &v)
+    {
+        // top, left , bottom, right pointer to make matrix smaller .
+        int t = 0, b = v.size() - 1, l = 0, r = v[0].size() - 1;
         vector<int> ans;
-        int c=1;
-        int dir=0;
-        while(t<=b && l<=r){
-            if(dir==0){
-                for(int i=l;i<=r;i++){
-                ans.push_back(v[t][i]);
+        int c = 1;
+        // direction variable
+        int dir = 0;
+        while (t <= b && l <= r)
+        {
+            // left to right traversal
+            if (dir == 0)
+            {
+                for (int i = l; i <= r; i++)
+                {
+                    ans.push_back(v[t][i]);
+                }
+                // increasing top var so that , that row is not visited again.
+                t++;
             }
-                            t++;
+            // for top to bottom
+            if (dir == 1)
+            {
+                for (int i = t; i <= b; i++)
+                {
+                    ans.push_back(v[i][r]);
+                }
+                r--;
             }
-
-            if(dir==1)
-{            for(int i=t;i<=b;i++){
-                ans.push_back(v[i][r]);
+            // for right to left
+            if (dir == 2)
+            {
+                for (int i = r; i >= l; i--)
+                {
+                    ans.push_back(v[b][i]);
+                }
+                b--;
             }
-            r--;}if(dir==2)
-            {for(int i=r;i>=l;i--){
-                ans.push_back(v[b][i]);
+            // for bottom to top
+            if (dir == 3)
+            {
+                for (int i = b; i >= t; i--)
+                {
+                    ans.push_back(v[i][l]);
+                }
+                l++;
             }
-            b--;}
-            if(dir ==3){for(int i=b;i>=t;i--){
-                ans.push_back(v[i][l]);
-            }
-            l++;}
-            dir=(++dir)%4;
+            dir = (++dir) % 4;
         }
         return ans;
     }
