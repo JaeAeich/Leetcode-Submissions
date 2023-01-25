a10f14b9c2b78297c0f93b984cee16d0d8190f86
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-01-25 12:48:21 +0530

    Time: 24 ms (58.73%), Space: 19.5 MB (70.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
-    //brute force ,using hash map
+    // //brute force ,using hash map
+    // int majorityElement(vector<int>& v) {
+    //     map<int,int> m;
+    //     //counting freq
+    //     for(int val:v){
+    //         m[val]++;
+    //     }
+    //     int maj_num=NULL;
+    //     int freq=0;
+    //     //Looping through map and finding the max frequency.
+    //     for(auto it=begin(m);it!=end(m);it++){
+    //         if(!maj_num || (maj_num<it->first && freq<it->second)){
+    //             maj_num=it->first;
+    //             freq=it->second;
+    //         }
+    //     }
+    //     return maj_num;
+    // }
+    
+    //Moore's voting algo
     int majorityElement(vector<int>& v) {
-        map<int,int> m;
-        //counting freq
-        for(int val:v){
-            m[val]++;
-        }
-        int maj_num=NULL;
-        int freq=0;
-        //Looping through map and finding the max frequency.
-        for(auto it=begin(m);it!=end(m);it++){
-            if(!maj_num || (maj_num<it->first && freq<it->second)){
-                maj_num=it->first;
-                freq=it->second;
+        //assume first ele to be maj with freq 1.
+        int maj_ele = v[0],c=1;
+        /*
+        loop thought the array if an ele with higher freq exists update maj_ele 
+        and c.
+        */
+        for(int i=1;i<v.size();i++){
+            //if c becomes zero then curr ele is maj ele.
+            if(c==0){
+                maj_ele=v[i];
             }
+            //if curr ele is same as maj ele , increase freq.
+            if(maj_ele==v[i]){
+                c++;
+            }
+            //else decrease freq.
+            else c--;
         }
-        return maj_num;
+        return maj_ele;
     }
+
 };
