99d4d5f475516a33bc60ff83d86685c2975c817c
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-01-27 21:33:38 +0530

    Time: 30 ms (60.81%), Space: 25.2 MB (23.83%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,5 +1,41 @@
 class Solution {
 public:
+    // //with div operator
+    // vector<int> productExceptSelf(vector<int>& v) {
+    //     long long int p=1,c=0;
+    //     for(int i=0;i<v.size();i++){
+    //         // taking product of all nums but zero.
+    //         if(v[i]) p*=v[i];
+    //         //counting number of zeroes.
+    //         else c++;
+    //     }
+    //     if(c<2){
+    //         // If one zero is present then except that zero all ele will be zero.
+    //         if(c==1){
+    //             for(int i=0;i<v.size();i++){
+    //                 if(!v[i]){
+    //                     v[i]=p;
+    //                 }
+    //                 else v[i]=0;
+    //             }
+    //         }
+    //         // if number of zeroes is 1 then each ele=total product/ele.
+    //         if(c==0){
+    //             for(int i=0;i<v.size();i++){
+    //                 v[i]=p/v[i];
+    //             }
+    //         }
+    //     }
+    //     // if there are more than 2 zeroes, all ele will become zeroes.
+    //     else{
+    //         for(int i=0;i<v.size();i++){
+    //             v[i]=0;
+    //         }
+    //     }
+    //     return v;
+    // }
+    
+    //without div operator
     vector<int> productExceptSelf(vector<int>& v) {
         int n=v.size();
         //creating a prefix product array
