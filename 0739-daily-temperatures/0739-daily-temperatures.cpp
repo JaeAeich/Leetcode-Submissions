38b5c055722e334f758c615dbb88586b08f0cf5c
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-01-30 23:47:54 +0530

    Time: 208 ms (89.27%), Space: 103.1 MB (27.85%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& v) {
-        //finding nge
+        //finding nge, if exists then ans is difference between the indices of this ele and nge.
         stack<int> s;
-        vector<int> nge (v.size(),-1);
+        vector<int> ans (v.size(),0);
         for(int i=0;i<v.size();i++){
             while(!s.empty() && v[s.top()]<v[i]){
-                nge[s.top()]=i;
+                ans[s.top()]=i-s.top();
                 s.pop();
             }
             s.push(i);
-        }
-        //checking when the nge comes after this ele.
-        vector<int> ans(v.size());
-        for(int i=0;i<v.size();i++){
-            if(nge[i]!=-1){
-                ans[i]=nge[i]-i;
-            }
-            //if this ele doesnt have nge then ans[i]=0 (according to ques).
-            else{
-                ans[i]=0;
-            }
         }
         return ans;
     }
