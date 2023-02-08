6b044ac1f64f572a115faf6a4376c681c1ae9c65
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-02-08 23:36:04 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (30.29%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    // //Imlementing lower_bound.
+    // int searchInsert(vector<int>& v, int t) {
+    //     int n=v.size();
+    //     int lo=0,hi=n-1,ans;
+    //     if(t<v[0]){
+    //         return 0;
+    //     }
+    //     if(v.back()<t){
+    //         return n;
+    //     }
+    //     //Implementation of stl lower_bound.
+    //     while(hi>=lo){
+    //         int mid=lo+(hi-lo)/2;
+    //         if(v[mid]>=t){
+    //             ans=mid;
+    //             hi=mid-1;
+    //         }
+    //         else{
+    //             lo=mid+1;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    
+    //using lower_bound
+    int searchInsert(vector<int>& v, int t){
+        int idx=lower_bound(begin(v),end(v),t)-begin(v);
+        return idx;
+    }
+};
