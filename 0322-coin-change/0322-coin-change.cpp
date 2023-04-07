abc95c4e24aabd6482335f0a03f1a4af4961de14
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-04-07 17:39:19 +0530

    Time: 84 ms (78.44%), Space: 15.4 MB (48.12%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -19,39 +19,57 @@ public:
     //     return ans ;
     // }
     
-    //dp
-    long helper(vector<int>& v,vector<vector<int>>& dp, long t, long i=0){
-        //if t becomes 0 it means that, this is a possibility. 
+//     //dp
+//     long helper(vector<int>& v,vector<vector<int>>& dp, long t, long i=0){
+//         //if t becomes 0 it means that, this is a possibility. 
+//         if(t==0){
+//             return 0;
+//         }
+//         //not a possiblity
+//         if(t<0 || i>=v.size()){
+//             return INT_MAX;
+//         }
+        
+//         //if stored no need to calculate.
+//         if(dp[t][i]!=-1) return dp[t][i];
+        
+//         //All the possible ways to make that target
+//         long takeAndStay = 1 + helper (v,dp,t-v[i],i);
+//         long takeAndMove = 1 + helper (v,dp,t-v[i],i+1);
+//         long move = helper (v,dp,t,i+1);
+        
+//         //return minimum of all the values
+//         int ans = min(min(takeAndStay , takeAndMove) , move);
+//         return dp[t][i]=ans ;
+//     }
+    
+    //dp, loop
+    long min(long a, long b){
+        return a>b?b:a;
+    }
+    long helper(vector<int>& v,vector<int>& dp, long t){
         if(t==0){
             return 0;
         }
-        //not a possiblity
-        if(t<0 || i>=v.size()){
+        
+        if(t<0){
             return INT_MAX;
         }
         
-        //if stored no need to calculate.
-        if(dp[t][i]!=-1) return dp[t][i];
+        if(dp[t]!=-1){
+            return dp[t];
+        }
+        int ans= INT_MAX;
         
-        //All the possible ways to make that target
-        long takeAndStay = 1 + helper (v,dp,t-v[i],i);
-        long takeAndMove = 1 + helper (v,dp,t-v[i],i+1);
-        long move = helper (v,dp,t,i+1);
+        for(int i=0;i<v.size();i++){
+            ans=min(ans,1+helper(v,dp,t-v[i]));
+        }
         
-        //return minimum of all the values
-        int ans = min(min(takeAndStay , takeAndMove) , move);
-        return dp[t][i]=ans ;
+        return dp[t]=ans;
     }
-    /*
-    Constraints:
-
-        1 <= coins.length <= 12
-        1 <= coins[i] <= 2^31 - 1
-        0 <= amount <= 104
-
-    */
+    
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(amount+1,vector<int>(coins.size(),-1));
+        vector<int> dp(amount+1,-1);
         long ans = helper(coins,dp,amount);
         return ans>=INT_MAX?-1:ans ;
     }
