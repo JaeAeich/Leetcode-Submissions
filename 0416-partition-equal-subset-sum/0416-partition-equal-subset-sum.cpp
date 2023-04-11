cf138527cdd1e1d8005d0866a14e52108f071287
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-04-11 13:06:55 +0530

    Time: 140 ms (88.80%), Space: 114.1 MB (16.57%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    bool helper(vector<int> &v,vector<vector<int>> &dp,int t, int i=0, int sum=0){
-        if(sum==t) return true;
-        if(i>=v.size() || t<sum){
-            return false;
-        }
-        if(dp[i][sum]==-1){
-            bool ans1=helper(v,dp,t,i+1,sum+v[i]);
-            if(ans1) {
-                return dp[i][sum]=1;
-            }
-            bool ans2=helper(v,dp,t,i+1,sum);
-            if(ans2) {
-                return dp[i][sum]=1;
-            }
-        }
-        return dp[i][sum]=0;
+    bool helper(vector<int> &v,vector<vector<int>> &dp,int sum, int tot=0, int i=0){
+        if(i>=v.size() || tot>sum) return 0;
+        if(sum==tot) return 1;
+        
+        if(dp[tot][i]!=-1) return dp[tot][i];
+        
+        return dp[tot][i]=helper(v,dp,sum,tot+v[i],i+1) || helper(v,dp,sum,tot,i+1);
     }
     
-    
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(begin(nums),end(nums),0);
-        vector<vector<int>> dp(nums.size()+1,vector<int>(21000,-1));
-        if(sum%2!=0){
-            return false;
-        }
+        int sum = accumulate ( begin(nums),end(nums),0);
+        if(sum%2!=0) return 0;
+        
+        int maxx = *max_element(begin(nums),end(nums));
+        vector<vector<int>> dp(sum/2+maxx+1,vector<int>(nums.size(),-1));
+        
         return helper(nums,dp,sum/2);
     }
 };
