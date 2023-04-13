a57443ff4a9a99c611e95b94202db81256eeb022
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-04-13 10:02:30 +0530

    Time: 59 ms (19.75%), Space: 21.5 MB (11.79%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -10,9 +10,32 @@ public:
         return dp[t][i]=take + move;
     }
     
+   int helperTab(vector<int> &v, int t) {
+    int n = v.size();
+    vector<vector<int>> dp(t+1, vector<int>(n+1));
+    
+    // Base case
+    for (int i = 0; i <= n; i++) {
+        dp[0][i] = 1;
+    }
+    
+    // Fill the table in bottom-up manner
+    for (int i = 1; i <= t; i++) {
+        for (int j = 1; j <= n; j++) {
+            int take = (i >= v[j-1]) ? dp[i - v[j-1]][j] : 0;
+            int move = dp[i][j-1];
+            dp[i][j] = take + move;
+        }
+    }
+    
+    // Return the result
+    return dp[t][n];
+}
+    
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(amount+1,vector<int>(n,-1));
-        return helper(coins,dp,amount,n-1);
+        // vector<vector<int>> dp(amount+1,vector<int>(n,-1));
+        // return helper(coins,dp,amount,n-1);
+        return helperTab(coins,amount);
     }
 };
