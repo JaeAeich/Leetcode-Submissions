ed00fae2dc1be8dab6ff756829e8d4e406eb228c
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-04-27 15:18:01 +0530

    Time: 1089 ms (5.01%), Space: 406.3 MB (5.47%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int helper(vector<vector<int>> &dp,string a, string b, int i=0, int j=0){
+        //if a if finised then we have to add all the characters of a, and if b if finished then delete all the characters of a.
         if(j>=b.size()) return a.size()-i;
         if(i>=a.length()) return b.size()-j;
-        
+        //memoize
         if(dp[i][j]!=-1) return dp[i][j];
-        
+        //all possibility
         int take = INT_MAX;
         if(a[i]==b[j]){
             take = helper(dp,a,b,i+1,j+1);
@@ -16,7 +17,7 @@ public:
         if(a[i]!=b[j]){
             rep = 1 + helper(dp,a,b,i+1,j+1);
         }
-        
+        //take min
         return dp[i][j]=min(min(min(take,del),ins),rep);
     }
     
