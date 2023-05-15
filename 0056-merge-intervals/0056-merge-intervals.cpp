9e6d9072dc0e14832300df7bf9670c37d001a100
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-05-15 20:20:29 +0530

    Time: 45 ms (40.60%), Space: 18.9 MB (84.83%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
-    static bool cmp(vector<int> &a, vector<int> &b){
-        return a[0]<b[0];
-    }
-    
     vector<vector<int>> merge(vector<vector<int>>& v) {
-        //sort it wrt starting time.
-        sort(begin(v),end(v),cmp);
-        vector<vector<int>> ans={};
-        int s=v[0][0];
-        int e=v[0][1];
-        /*
-        If the ith interval starts before the perevious interval
-        ends then that means there is an overlap, if this interval 
-        ends after prev interval then we will update the ending time
-        as the inteval has been merged.
-        */
-        for(int i=0;i<v.size();i++){
-            if(v[i][0]<=e){
-                if(v[i][1]>e) e=v[i][1];
+        //sort intervals wrt starting time.
+        sort(begin(v),end(v),[](vector<int> &a, vector<int> &b) {return a[0]<b[0];});
+        //Push the first interval as it will be in the ans and update it if needed.
+        vector<vector<int>> ans = {v[0]};
+        
+        //Iterate though the intervals and check is this interval overlaps with the prev.
+        for(int i=1;i<v.size();i++){
+            int s = v[i][0];
+            int e = v[i][1];
+            //if overlaps.
+            if(ans.back()[1]>=s){
+                if(e>ans.back()[1]){
+                    ans.back()[1]=e;
+                }
             }
+            //else push a new interval.
             else{
-                ans.push_back({s,e});
-                s=v[i][0];
-                e=v[i][1];
+                ans.push_back(v[i]);
             }
         }
-        /*
-        We will push the last entry as well as, no matter
-        which loop the last interval ends up ie first or second,
-        it will not be pushed.
-        */
-        ans.push_back({s,e});
         return ans;
     }
 };
