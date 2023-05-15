efb79ffffda134e0551730ba31c3f1b28904913e
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-05-15 19:59:17 +0530

    Time: 45 ms (40.60%), Space: 19 MB (68.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,10 +5,17 @@ public:
     }
     
     vector<vector<int>> merge(vector<vector<int>>& v) {
+        //sort it wrt starting time.
         sort(begin(v),end(v),cmp);
         vector<vector<int>> ans={};
         int s=v[0][0];
         int e=v[0][1];
+        /*
+        If the ith interval starts before the perevious interval
+        ends then that means there is an overlap, if this interval 
+        ends after prev interval then we will update the ending time
+        as the inteval has been merged.
+        */
         for(int i=0;i<v.size();i++){
             if(v[i][0]<=e){
                 if(v[i][1]>e) e=v[i][1];
@@ -19,6 +26,11 @@ public:
                 e=v[i][1];
             }
         }
+        /*
+        We will push the last entry as well as, no matter
+        which loop the last interval ends up ie first or second,
+        it will not be pushed.
+        */
         ans.push_back({s,e});
         return ans;
     }
