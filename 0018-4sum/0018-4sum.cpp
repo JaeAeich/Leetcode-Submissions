21a373a3fe422f5714f207d86655132de173360f
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-05-20 23:08:17 +0530

    Time: 76 ms (66.85%), Space: 13.1 MB (82.30%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -3,14 +3,18 @@ public:
     vector<vector<int>> fourSum(vector<int>& v, int t) {
         int n = v.size();
         vector<vector<int>> ans;
+        // sort the array to use two pointers
         sort(begin(v),end(v));
         for(int i=0;i<n;i++){
+            // if already taken this as the first ele, skip
             if(i>0 && v[i]==v[i-1]) continue;
+            // if already take this as the second ele, skip
             for(int j=i+1;j<n;j++){
                 if(j!=i+1 && v[j]==v[j-1]) continue;
                 int k = j+1;
                 int l = n-1;
                 while(k<l){
+                    // add incrementally other wise overflow might happen
                     long long int sum = v[i] + v[j];
                     sum+=v[k];
                     sum+=v[l];
@@ -22,6 +26,7 @@ public:
                     }
                     else{
                         ans.push_back({v[i],v[j],v[k],v[l]});
+                        //store the vals so that skip over the same ones
                         int kval = v[k],lval=v[l];
                         do{
                             k++;
