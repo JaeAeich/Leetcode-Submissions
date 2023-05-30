8502e77931542195e41dd0b7698d11f2dbf41743
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-05-30 11:21:22 +0530

    Time: 150 ms (35.93%), Space: 93.3 MB (41.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& v) {
-        //Atleast the ans will 0, ie sold on the same day.
+        // Profit will atleast be 0, sell on the same day
         int ans = 0;
+        
+        // We to keep track of the lowest price on the prev days.
         int prev = v[0];
-        for(int i = 1;i<v.size();i++){
-            //if selling on the ith day is no profitable, this means it better to buy on this day.
-            if(v[i]-prev<0){
-                prev=v[i];
-            }
-            ans = max(ans,v[i]-prev);
+        
+        // Iterate 
+        for(int i=1;i<v.size();i++){
+            // If the profit from buying on the prev lowest val
+            // and selling today is greater than other, update the ans. 
+            ans = max(v[i]-prev,ans);
+            
+            // If price of this day is smaller that prev, update.
+            if(v[i]<prev) prev=v[i];
         }
         return ans;
     }
