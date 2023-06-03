0aaab05d67875aa38365ead652e4dc84422a4961
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-03 18:57:52 +0530

    Time: 4 ms (95.32%), Space: 7.4 MB (47.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int> m1;
-        map<char,int> m2;
-        for(auto c: s) m1[c]++;
-        for(auto c: t) m2[c]++;
+        // freq count
+        vector<int> m1(26,0);
+        vector<int> m2(26,0);
+        
+        // count freq
+        for(auto c: s) m1[c-'a']++;
+        for(auto c: t) m2[c-'a']++;
+        
+        // If freq of each char is equal in both, then they are anagram
         return m1==m2;
     }
 };
