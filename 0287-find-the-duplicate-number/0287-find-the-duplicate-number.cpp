c497c70d12bdef7ca07ca9ac73de11c90e2f542f
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-05 12:11:35 +0530

    Time: 119 ms (69.49%), Space: 61.3 MB (29.00%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        map<int,int> m;
-        for (auto n : nums) {
-            m[n]++;
-            if(m[n]>1) return n;
+    // Using hare, tortise method.
+    // Detecting cycle in LL
+    int findDuplicate(vector<int>& v) {
+        // Slo and fast pointers
+        int slo = v[0], fast = v[0];
+        
+        // Find the first collision
+        do{
+            slo = v[slo];
+            fast = v[v[fast]];
+        }while(slo!=fast);
+        
+        slo = v[0];
+        
+        // Find the beginning of cycle
+        while(slo!=fast){
+            slo = v[slo];
+            fast = v[fast];
         }
-        return -1;
+        return slo;
     }
 };
