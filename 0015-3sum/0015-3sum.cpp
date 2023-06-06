342b600c150b1542be8656b27211668d56e49376
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-06 17:59:21 +0530

    Time: 116 ms (95.65%), Space: 24.2 MB (63.42%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& v) {
-        int n = v.size();
+        int n=v.size();
+        
+        // Vector to store the answer
         vector<vector<int>> ans;
-        // sort the array to use two pointers.
+        
+        // Sort to use two-poninter approach
         sort(begin(v),end(v));
-        for(int i=0;i<n;i++){
-            // if this ele has been processed move.
-            if(i>0 && v[i]==v[i-1]) continue;
-            int j=i+1,k=n-1;
-            while(j<k){
-                int sum = v[i]+v[j]+v[k];
-                if(sum>0){
-                    k--;
-                }
-                else if(sum<0){
-                    j++;
-                }
-                else{
-                    ans.push_back({v[i],v[j],v[k]});
-                    int jval=v[j],kval=v[k];
-                    /* 
-                    move atleast once and then if still at the same ele to avoid redundancy 
-                    keep moving till a different ele is found.
-                    */
+        
+        // Till n-2 because after that its impossible to make vec of 3 ele
+        for(int i=0;i<n-2;){
+            int vi = v[i]; 
+            
+            // j and k are two pointers
+            for(int j=i+1,k=n-1;j<k;){
+                int vj = v[j];
+                int vk = v[k];
+                int sum = vi + vj + vk;
+                
+                // If required answer found
+                if(sum==0){
+                    ans.push_back({vi,vj,vk});
+                    
+                    // Since we can't have duplicate answers, don't consider equal ele
                     do{
                         j++;
-                    }while(j<k && v[j]==jval);
+                    }while(j<=k && v[j]==vj);
                     do{
                         k--;
-                    }while(k>j && v[k]==kval);
+                    }while(k>=j && v[k]==vk);
                 }
+                else if(sum < 0) j++;
+                else k--;
             }
+            
+            // To avoid dup, avoid same ele
+            do{
+                i++;
+            }
+            while(i<n-2 && v[i]==vi);
         }
         return ans;
     }
