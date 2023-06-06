83b2355e045374e7d192687b4e04e03441caa3b1
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-06 18:52:53 +0530

    Time: 84 ms (60.84%), Space: 13.2 MB (57.52%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,39 +2,38 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& v, int t) {
         int n = v.size();
-        vector<vector<int>> ans;
-        // sort the array to use two pointers
+        vector<vector<int>> ans = {};
+        
+        // Sort to use two-pointer
         sort(begin(v),end(v));
-        for(int i=0;i<n;i++){
-            // if already taken this as the first ele, skip
-            if(i>0 && v[i]==v[i-1]) continue;
-            // if already take this as the second ele, skip
-            for(int j=i+1;j<n;j++){
-                if(j!=i+1 && v[j]==v[j-1]) continue;
-                int k = j+1;
-                int l = n-1;
+        
+        for(int i=0;i<n-3;i++){
+            
+            // Remove duplicate value
+            if(i && v[i]==v[i-1]) continue;
+            for(int j=i+1;j<n-2;j++){
+                
+                // Remove duplicate value
+                if(j>i+1 && v[j]==v[j-1]) continue;
+                int k = j+1, l = n-1;
                 while(k<l){
-                    // add incrementally other wise overflow might happen
-                    long long int sum = v[i] + v[j];
+                    
+                    // Don't use sum = v[i]+v[j]+v[k]+v[l]
+                    long long int sum = v[i];
+                    sum+=v[j];
                     sum+=v[k];
                     sum+=v[l];
-                    if(sum<t){
-                        k++;
-                    }
-                    else if(sum>t){
-                        l--;
-                    }
-                    else{
+                    if(sum==t){
                         ans.push_back({v[i],v[j],v[k],v[l]});
-                        //store the vals so that skip over the same ones
-                        int kval = v[k],lval=v[l];
                         do{
                             k++;
-                        }while(k<l && v[k]==kval);
+                        }while(k<=l && v[k]==v[k-1]);
                         do{
                             l--;
-                        }while(l>k && v[l]==lval);
+                        }while(k<=l && v[l]==v[l+1]);
                     }
+                    else if(sum<t) k++;
+                    else l--;
                 }
             }
         }
