5ae7e6b506d73616f57a3a5957953a210921919b
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-07 11:02:59 +0530

    Time: 104 ms (82.96%), Space: 43.6 MB (78.19%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+    // Using sorting and counting the freq
     int longestConsecutive(vector<int>& v) {
-        if(!v.size()) return 0;
+        if(v.size()<=1) return v.size();
         sort(begin(v),end(v));
         int ans = 1;
-        int c=1;
+        int f=1;
         for(int i=1;i<v.size();i++){
             if(v[i]==v[i-1]) continue;
-            if(v[i]==v[i-1]+1){
-                c++;
-                ans=max(ans,c);
+            // If consecutve count the freq
+            if(v[i]-v[i-1]==1){
+                f++;
+                ans=max(ans,f);
             }
+            // If a non-consecutive ele found, start a new subsequence
             else{
-                c=1;
+                f=1;
             }
         }
         return ans;
