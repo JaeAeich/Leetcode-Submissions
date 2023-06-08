0b185c63c97e9202a4e70beb7436604a7348c047
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-09 01:57:33 +0530

    Time: 12 ms (34.02%), Space: 14.7 MB (97.82%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        // Obv cases
+        if(!l1) return l2;
+        if(!l2) return l1;
+        if(!l1 && !l2) return nullptr;
+        
+        // Three pointers
+        ListNode *curr = nullptr, *p1 =nullptr, *p2 = nullptr;
+        
+        // Setting up pointers so the curr pointer is the least
+        if(l1->val<=l2->val){
+            curr = l1;
+        }
+        else {
+            curr = l2;
+        }
+        p1 = curr->next;
+        p2 = curr==l1?l2:l1;
+        
+        // Since curr is least it will be the ans
+        ListNode *ans = curr;
+        
+        // Till either of the list is exhaused
+        while(p1 && p2){
+            
+            // Among the two values choose the smaller one
+            if(p2->val<=p1->val){
+                ListNode* temp = p2;
+                p2 = p2->next;
+                curr->next = temp;
+                curr = temp;
+            }
+            else{
+                ListNode*temp = p1;
+                p1 = p1->next;
+                curr->next = temp;
+                curr = temp;
+            }
+        }
+        
+        // If any one of the list is left attach it 
+        if(p1){
+            curr->next = p1;
+        }
+        else if(p2) curr->next = p2;
+        
+        return ans;
+    }
+};
