d505415861d2c7025c4181a9578f7959bbebcbc7
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-10 13:40:50 +0530

    Time: 8 ms (89.09%), Space: 18.3 MB (91.46%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& v) {
+        int n = v.size();
+        
+        // If the len is less that 1 then no dup present
+        if(n<=1) return n;
+        
+        // Atleast 1 non dup present
+        // Also because the loop start from 1 not 0
+        int ans = 1;
+        int dupSoFar = 0;
+        
+        for(int i=1; i<n;i++){
+            // If prev ele is equal to this ele
+            // Increase the count of dup
+            if(v[i]==v[i-1]){
+                dupSoFar++;
+            }
+            else{
+                // If dup not present
+                // Then move this ele to the index till where there is no dup
+                v[i-dupSoFar]  = v[i];
+                ans++;
+            }
+        }
+        
+        return ans ;
+    }
+};
