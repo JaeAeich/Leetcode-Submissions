4656956116722884f9a9f58514d497363a6ca7d1
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-13 23:13:03 +0530

    Time: 70 ms (34.80%), Space: 45 MB (30.02%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
@@ -2,6 +2,8 @@ type Fn = (accum: number, curr: number) => number
 
 function reduce(nums: number[], fn: Fn, init: number): number {
     let ans = init;
-    nums.forEach((val) => ans = fn(ans,val));
+    for(let i=0;i<nums.length;i++){
+        ans = fn(ans,nums[i]);
+    }
     return ans;
 };
