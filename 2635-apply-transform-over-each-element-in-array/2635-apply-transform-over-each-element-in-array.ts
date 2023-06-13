975c9ab41c4a63dab73b0d0044aee96ff9a1a205
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-13 22:17:47 +0530

    Time: 65 ms (40.16%), Space: 42.7 MB (81.65%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
@@ -1,3 +1,6 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-    return arr.map(fn);
+    for(let i = 0; i<arr.length; i++){
+        arr[i]=fn(arr[i],i);
+    }
+    return arr;
 };
