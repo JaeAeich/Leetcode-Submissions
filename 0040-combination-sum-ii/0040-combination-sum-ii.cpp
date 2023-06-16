a312df600dbd252598a1868741be11e7c27ef316
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-16 17:16:47 +0530

    Time: 9 ms (43.26%), Space: 11.5 MB (23.16%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     vector<vector<int>> finans = {};
     
-    void helper(vector<int>& v, int t, vector<int> ans={}, int i=0){
-        // Base case
-        if(i>=v.size() or t<0){
-            if(!t) finans.push_back(ans);
+    void helper(vector<int> &v, int t, int idx=0,vector<int> ans={}){
+        if(t<0 || idx>v.size()) return;
+        if(t==0){
+            finans.push_back(ans);
             return ;
         }
         
-        // Pick this ele and move
-        ans.push_back(v[i]);
-        helper(v,t-v[i],ans,i+1);
-        ans.pop_back();
-        
-        // Don't pick this ele ever
-        while(i+1<v.size() && v[i]==v[i+1]) i++;
-        helper(v,t,ans,i+1);
+        for(int i=idx;i<v.size();++i){
+            if(t-v[i]<0) return;
+            ans.push_back(v[i]);
+            helper(v,t-v[i],i+1,ans);
+            while(i+1<v.size() && v[i]==v[i+1]) i++;
+            ans.pop_back();
+        }
     }
     
     vector<vector<int>> combinationSum2(vector<int>& v, int t) {
