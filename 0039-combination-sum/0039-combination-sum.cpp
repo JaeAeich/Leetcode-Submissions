0e91df43a94049a496083172b375d1d7763849fa
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-16 16:56:06 +0530

    Time: 109 ms (12.80%), Space: 69.6 MB (8.75%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> finans = {};
+    
+    void helper(vector<int>& v, int t, vector<int> ans = {}, int i=0){
+        if(i>=v.size() or t<=0){
+            if(!t) finans.push_back(ans);
+            return ;
+        }
+        // Pick the ith ele and possibly again pick it
+        ans.push_back(v[i]);
+        helper(v,t-v[i],ans,i);
+        
+        // Don't pick this ele and move on
+        ans.pop_back();
+        helper(v,t,ans,i+1);
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& v, int t) {
+        helper(v,t);
+        return finans;
+    }
+};
