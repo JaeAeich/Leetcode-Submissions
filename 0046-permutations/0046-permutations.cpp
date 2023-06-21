cac4a8ad8c599eda2aa514bd255fd01584e30b3a
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-06-21 21:55:25 +0530

    Time: 7 ms (26.97%), Space: 8.5 MB (8.89%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:
+    // Final ans to be returned
+    vector<vector<int>> finans = {};
+    
+    // Calculate the permutations
+    void helper(vector<int>& v, vector<bool> &taken, vector<int> ans = {}){
+        if(ans.size() == v.size()){
+            finans.emplace_back(ans);
+            return;
+        }
+        
+        for(int i=0; i<v.size(); i++){
+            // If this ele is not taken 
+            if(!taken[i]){
+                
+                // Mark it as taken
+                taken[i] = 1;
+                
+                // Take it into answer as this is one of the perms
+                ans.push_back(v[i]);
+                helper(v, taken, ans);
+                
+                // Backtrack to generate other possibilities
+                ans.pop_back();
+                taken[i] = 0;
+            }
+        }
+    }
+public:
+    // Driver code
+    vector<vector<int>> permute(vector<int>& v) {
+        vector<bool> taken(v.size());
+        helper(v, taken);
+        return finans;
+    }
+};
