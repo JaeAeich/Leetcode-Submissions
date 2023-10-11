aaaf71475592e6a5467cddf94988fd55df4d19f3
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-10-12 00:00:24 +0530

    Time: 261 ms (5.03%), Space: 127.4 MB (5.13%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
-public:
-    vector<vector<int>> finans = {};
-    
-    void helper(vector<int> &v, int t, int idx=0,vector<int> ans={}){
-        if(t<0 || idx>v.size()) return;
-        if(t==0){
-            finans.push_back(ans);
-            return ;
+private:
+    vector<vector<int>> ans;
+
+    void helper(vector<int> v, int t, vector<int> ans_here = {}, int i=0){
+        if(t == 0){
+            ans.push_back(ans_here);
+            return;
         }
-        
-        for(int i=idx;i<v.size();++i){
-            if(t-v[i]<0) return;
-            ans.push_back(v[i]);
-            helper(v,t-v[i],i+1,ans);
-            while(i+1<v.size() && v[i]==v[i+1]) i++;
-            ans.pop_back();
-        }
+
+        if(i >= v.size() || t < 0) return;
+
+        // Take this ele
+        ans_here.push_back(v[i]);
+        helper(v, t - v[i], ans_here, i+1);
+        ans_here.pop_back();
+
+        // Don't take this ele and skip all its occurances
+        int j = i + 1;
+        while(j < v.size() && v[i] == v[j]) j++;
+        helper(v, t, ans_here, j);
     }
-    
+
+public:
     vector<vector<int>> combinationSum2(vector<int>& v, int t) {
-        // Sort the array to remove dups
-        sort(begin(v),end(v));
-        helper(v,t);
-        return finans;
+        sort(begin(v), end(v));
+        helper(v, t);
+        return ans;
     }
 };
