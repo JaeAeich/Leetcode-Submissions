d57b9d09e87166fbed327df3e64b5b234a1f9048
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-10-12 11:34:49 +0530

    Time: 115 ms (10.06%), Space: 6.3 MB (39.58%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.cpp
+++ 0060-permutation-sequence/0060-permutation-sequence.cpp
@@ -1,37 +1,47 @@
 class Solution {
-private:
-    void helper(string &s){
-        int n = s.length();
+private: 
+    // Returns the next permutation of the string in lexicographical order
+    void nextPerm(string &s){
         int piv = -1;
-        for(int i=n-2;i>=0;i--){
-            if(s[i]<s[i+1]){
+
+        /*
+        Find the first small character from right
+        (comparing characters directly)
+        */
+        for(int i = s.length() - 2; i >= 0; i--){
+            if(s[i] < s[i+1]){
                 piv = i;
                 break;
             }
         }
-        
-        if(piv == -1) {
-            reverse(begin(s),end(s));
-            return ;
+
+        /*
+        If the pivot is -1, reverse the entire string
+        and return.
+        */
+        if(piv == -1){
+            reverse(begin(s), end(s));
+            return;
         }
-        
-        for(int i=n-1;i>=piv;i--){
-            if(s[i]>s[piv]){
-                swap(s[i],s[piv]);
+
+        for(int i = s.length() - 1; i > piv; i--){
+            if(s[i] > s[piv]){
+                swap(s[i], s[piv]);
                 break;
             }
         }
         
-        reverse(begin(s)+piv+1,end(s));
+        reverse(s.begin() + piv + 1, end(s));
     }
 public:
     string getPermutation(int n, int k) {
-        string s="";
-        for(int i=1;i<=n;i++){
-            s+=to_string(i);
+        string s = "";
+        k--;
+        for(int i = 1; i <= n; i++){
+            s += to_string(i);
         }
-        while(--k){
-            helper(s);
+        while(k--){
+            nextPerm(s);
         }
         return s;
     }
