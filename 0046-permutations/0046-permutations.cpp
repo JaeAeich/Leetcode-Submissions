9af4ff2d95dfdef38e7ffa383c80fcd623557022
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-10-12 12:15:40 +0530

    Time: 5 ms (30.41%), Space: 8.3 MB (24.73%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,38 +1,29 @@
 class Solution {
 private:
-    // Final ans to be returned
-    vector<vector<int>> finans = {};
-    
-    // Calculate the permutations
-    void helper(vector<int>& v, vector<bool> &taken, vector<int> &ans){
-        if(ans.size() == v.size()){
-            finans.emplace_back(ans);
+    vector<vector<int>> finans;
+
+    void helper(vector<int>& v, vector<bool>& vis, vector<int>& ans) {
+        if (ans.size() == v.size()) {
+            finans.push_back(ans);
             return;
         }
-        
-        for(int i=0; i<v.size(); i++){
-            // If this ele is not taken 
-            if(!taken[i]){
-                
-                // Mark it as taken
-                taken[i] = 1;
-                
-                // Take it into answer as this is one of the perms
+        for (int i = 0; i < v.size(); i++) {
+            if (!vis[i]) {
                 ans.push_back(v[i]);
-                helper(v, taken, ans);
-                
-                // Backtrack to generate other possibilities
+                vis[i] = true;
+                helper(v, vis, ans);
+                vis[i] = false;
                 ans.pop_back();
-                taken[i] = 0;
             }
         }
     }
+
 public:
-    // Driver code
-    vector<vector<int>> permute(vector<int>& v) {
-        vector<bool> taken(v.size());
-        vector<int> ans = {};
-        helper(v, taken, ans);
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> vis(n, false);
+        vector<int> ans;
+        helper(nums, vis, ans);
         return finans;
     }
 };
