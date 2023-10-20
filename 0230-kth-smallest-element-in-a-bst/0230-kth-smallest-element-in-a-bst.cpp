0864c1b1b5f62db9f088c0e6d1412333dee42ad7
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-10-20 20:27:41 +0530

    Time: 11 ms (81.20%), Space: 24.6 MB (18.48%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -11,16 +11,20 @@
  */
 class Solution {
 public:
-    vector<int> ans;
+    int c;
+    int ans = -1;
     
     void helper(TreeNode* root){
         if(!root) return;
         helper(root->left);
-        ans.push_back(root->val);
+        c--;
+        if(c==0) ans = root->val;
         helper(root->right);
     }
+    
     int kthSmallest(TreeNode* root, int k) {
+        c = k;
         helper(root);
-        return ans[k-1];
+        return ans;
     }
 };
