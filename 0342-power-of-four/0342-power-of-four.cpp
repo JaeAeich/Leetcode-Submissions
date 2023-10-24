f9c7ce47ac2c37831625139a59d3f0b4d0888824
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-10-24 23:18:50 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (27.58%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -2,7 +2,11 @@ class Solution {
 public:
     bool isPowerOfFour(int n) {
         if(n <=0 ) return false;
+        
+        // Need to be a power of 2
         if(n&(n-1)) return false;
+        
+        // Set bit needs to be at odd position
         return n&0x555555555;
     }
 };
