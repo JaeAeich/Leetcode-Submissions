a48bd51a6fb4e3b68cbe7bef3c2266e08f107c6d
Javed Habib <100477031+JaeAeich@users.noreply.github.com>
2023-11-04 23:07:31 +0530

    Time: 3 ms (87.88%), Space: 16.8 MB (5.57%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,51 +1,43 @@
 class Solution {
+private:
+    vector<pair<int, int>> moves = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
 public:
-    bool isValid(vector<vector<int>> &v, int i, int j){
-        return i >=0 && j>=0 && i < v.size() && j < v[0].size();
-    }
-    
-    // Defines the moves
-    vector<pair<int,int>> moves = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    
     int orangesRotting(vector<vector<int>>& v) {
+        // Get the size of grid
         int n = v.size(), m = v[0].size();
-        
-        // Stores the cordinate and the time elapsed till rotting this orange
-        queue<pair<pair<int,int>,int>> q;
-        for(int i = 0; i < n; i++){
-            for(int j =0; j < m; j++){
-                // if the i, j is rotten push it in queue
-                if(v[i][j] == 2){
-                    q.push({{i, j},0});
-                }
-            }
-        }
+        // Queue for BFS
+        queue<vector<int>> q;
+        // Max time taken to rott oranges
         int ans = 0;
-        while(!q.empty()){
-            auto cell = q.front();
-            q.pop();
-            int x = cell.first.first;
-            int y = cell.first.second;
-            int t = cell.second;
-            
-            
-            for(auto m : moves){
-                int x1 = x+m.first;
-                int y1 = y+m.second;
-                
-                
-                // If its neighbors are fresh rot them
-                if(isValid(v, x1, y1) && v[x1][y1] == 1){
-                    // Rott this orange
-                    v[x1][y1] = 2;
-                    q.push({{x1,y1},t+1});
-                    ans = max(ans, t + 1);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(v[i][j] == 2){
+                    // Get the starting points for BFS
+                    v[i][j] = 1;
+                    q.push({i, j, 0});
                 }
             }
         }
-        
+        while(!q.empty()){
+            auto vec = q.front();
+            q.pop();
+            int i = vec[0], j = vec[1], t = vec[2];
+            // check if this is a valid orange 
+            if(i < 0 || j < 0 || i >= n || j >= m || v[i][j] == 0 || v[i][j] == 2) continue;
+            
+            // Rott this orange
+            v[i][j] = 2;
+            
+            // Update the ans
+            ans = max(ans, t);
+            for(auto m : moves){
+                // Rott its neighbour
+                q.push({i + m.first, j  +m.second, t + 1});
+            }
+        }
         for(int i = 0; i < n; i++){
-            for(int j =0; j < m; j++){
+            for(int j = 0; j < m; j++){
                 if(v[i][j] == 1){
                     return -1;
                 }
